313727febc9e8bcdc40df84937461792f5a1a0a3
Mc Nigno <mcnigno@gmail.com>
2018-07-07 17:55:45 +0200

    projects data bind

--- fab_addon_suitecrm/fab_addon_suitecrm/models.py
+++ fab_addon_suitecrm/fab_addon_suitecrm/models.py
@@ -1,11 +1,11 @@
 from flask_appbuilder import Model
 from flask_appbuilder.models.mixins import AuditMixin, FileColumn, ImageColumn
-from sqlalchemy import Column, Integer, String, ForeignKey, Date
+from sqlalchemy import Column, Integer, String, ForeignKey, Date, Sequence
 from sqlalchemy.orm import relationship
  
 class Project(Model):
     __bind_key__ = 'suitecrm'
-    id = Column(Integer, primary_key=True)
+    id = Column(String(50), primary_key=True)
     name =  Column(String(50), nullable=False)
     assigned_user_id = Column(String(38))
     estimated_start_date = Column(Date)
