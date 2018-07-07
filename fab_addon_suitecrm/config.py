df846d934868642f7c203461e0e0b969b519c13d
Mc Nigno <mcnigno@gmail.com>
2018-07-07 15:30:17 +0200

    config suite crm database bind

--- fab_addon_suitecrm/config.py
+++ fab_addon_suitecrm/config.py
@@ -1,5 +1,5 @@
 ADDON_NAME='suitecrm'
-FULL_ADDON_NAME='fab_addon_' + ADDON_NAME
+FULL_ADDON_NAME='fab_addon_suitecrm.fab_addon_' + ADDON_NAME
 SQLALCHEMY_BINDS = {
     'suitecrm': 'mysql://bisio:lollipop300777@10.133.33.77/suitecrm'
 }
