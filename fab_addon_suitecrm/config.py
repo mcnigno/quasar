e84e0fb3e51f82a83e74fba3a11b5535cb3d97dd
Mc Nigno <mcnigno@gmail.com>
2018-07-20 20:26:40 +0200

    added metrics and doctype logic

--- fab_addon_suitecrm/config.py
+++ fab_addon_suitecrm/config.py
@@ -1,8 +1,9 @@
 ADDON_NAME='suitecrm'
 FULL_ADDON_NAME='fab_addon_' + ADDON_NAME
 SQLALCHEMY_BINDS = {
-    'suitecrm': 'mysql://bisio:lollipop300777@10.133.33.77/suitecrm',
-    'crm_ext': 'mysql://root:lollipop300777@localhost/crm_ext'
+    'crm_ext': 'mysql://root:lollipop300777@localhost/crm_ext',
+    'suitecrm': 'mysql://bisio:lollipop300777@10.133.33.77/suitecrm'
+    #'crm_ext': 'mysql://root:lollipop300777@localhost/crm_ext'
 }
 
 #ADDON_MANAGERS = ['fab_addon_suitecrm.manager.MyAddOnManager']
