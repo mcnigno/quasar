4ecfb7ffa2c335a6b3a7792c10ed789fb82dd1aa
Mc Nigno <mcnigno@gmail.com>
2018-07-20 18:22:04 +0200

    added metrics and doctype logic

--- fab_addon_suitecrm/fab_addon_suitecrm/models.py
+++ fab_addon_suitecrm/fab_addon_suitecrm/models.py
@@ -1,6 +1,6 @@
 from flask_appbuilder import Model
-from flask_appbuilder.models.mixins import AuditMixin, FileColumn, ImageColumn
-from sqlalchemy import Column, Integer, String, ForeignKey, Date, Sequence, CHAR
+from flask_appbuilder.models.mixins import AuditMixin, FileColumn, ImageColumn,
+from sqlalchemy import Column, Integer, String, ForeignKey, Date, Sequence, CHAR, Boolean, Text
 from sqlalchemy.orm import relationship
  
 class Project(Model):
@@ -12,6 +12,8 @@ class Project(Model):
     estimated_end_date = Column(Date)
     status = Column(String(38))
     deleted = Column(String(38))
+    metrics = Column(CHAR(36), ForeignKey('metrics.id'))
+    metric = relationship('Metrics')
 
     def __repr__(self):
         return self.name
@@ -25,3 +27,36 @@ class ProjectTask(Model):
     deleted = Column(String(38))
     project_id = Column(CHAR(36), ForeignKey('project.id'))
     project = relationship('Project')
+
+class Metrics(Model, AuditMixin):
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(Strig(50), nullable=False)
+    description = Column(Text)
+    active = Column(Boolean())
+    value = Column(Integer())
+    project_id = Column(CHAR(36), ForeignKey('project.id'))
+    project = relationship('Project')
+    doctype_id = Column(CHAR(36), ForeignKey('doctype.id'))
+    doctype = relationship('Doctype')
+
+
+class Doctype(Model, AuditMixin):
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(Strig(50), nullable=False)
+    description = Column(Text)
+
+class MetricSlug(model, AuditMixin):
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(Strig(50), nullable=False)
+    doctype_id = Column(CHAR(36), ForeignKey('doctype.id'))
+    doctype = relationship('Doctype')
+    date = Column(Date, nullable=False)
+    project_id = Column(CHAR(36), ForeignKey('project.id'))
+    project = relationship('Project')
+
+
+
+
+
+
+
--- fab_addon_suitecrm/fab_addon_suitecrm/views.py
+++ fab_addon_suitecrm/fab_addon_suitecrm/views.py
@@ -1,7 +1,7 @@
 from flask import render_template
 from flask_appbuilder.models.sqla.interface import SQLAInterface
 from flask_appbuilder import ModelView
-from .models import Project, ProjectTask
+from .models import Project, ProjectTask, Metrics, Doctype, MetricSlug
 
 """
     Create your Views (but don't register them here, do it on the manager::
@@ -16,12 +16,23 @@ class ProjectTaskView(ModelView):
     datamodel = SQLAInterface(ProjectTask)
     list_columns = ['id','name']
 
+class MetricsView(ModelView):
+    datamodel = SQLAInterface(Metrics)
+
+class DoctypeView(ModelView):
+    datamodel = SQLAInterface(Doctype)
+
+class MetricSlugView(ModelView):
+    datamodel = SQLAInterface(MetricSlug)
+
 class ProjectView(ModelView):
     datamodel = SQLAInterface(Project)
     list_columns = ['id', 'name', 'estimated_start_date','status']
     show_columns = ['id', 'name', 'estimated_start_date']
     edit_columns = ['id', 'name', 'estimated_start_date']
 
-    related_views = [ProjectTaskView]
+    related_views = [MetricsView, MetricSlugView]
+
+
     
 
